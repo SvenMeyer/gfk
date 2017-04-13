03bcbab7ab6eddb4496b51daae577d25fe1788aa
Sven Meyer <github_dev@svenmeyer.com>
2017-04-13 15:01:58 +0200

    filter events of panel member cookies v1.0

---
+++ addthis/addthis_preprocess_dir.py
@@ -0,0 +1,61 @@
+import os
+import glob
+import time
+from datetime import date, timedelta
+import gzip
+import json
+import pandas as pd
+
+HOME_DIR = "/media/sf_SHARE"
+if not os.path.isdir(HOME_DIR):
+    HOME_DIR = os.path.expanduser("~")
+WORK_DIR = os.path.join(HOME_DIR, "ML_DATA/GFK/AddThis/")
+input_dir = os.path.join(WORK_DIR , 'data')
+print("input_dir =", input_dir)
+output_dir = os.path.join(WORK_DIR , 'data_panel')
+print("output_dir =", output_dir)
+
+cookie_df  = pd.read_csv(os.path.join(WORK_DIR,'addthis_cookies_2017-04-12.csv'))
+cookie_set = set(cookie_df['addthis_uid'])
+
+day = date(2017,3,20)
+
+FILE_WILDCARD = "pixelview-turbo-no-porn-de." + day.strftime('%Y%m%d') + "-????.????.log.gz"
+data_files = glob.glob(os.path.join(input_dir, FILE_WILDCARD))
+data_files_count = len(data_files)
+print(data_files_count, "files found for day", day.strftime('%Y%m%d'))
+
+while data_files_count > 0:
+    output_file_name = os.path.join(output_dir, 'addthis_panel_' + day.strftime('%Y%m%d') + '.tsv')
+    output_file = text_file = open(output_file_name, "w")
+    print('TIMESTAMP\tUID\tGEO\tURL\tCATEGORIES\tUSERAGENT\tMETA_KEYWORDS\tKEY_TERMS\tENTITIES', file = output_file)
+    
+    n_cookies = 0
+    for datafile in data_files:
+        print("start processing file : ", datafile)
+        i=0
+        n=0
+        with gzip.open(os.path.join(input_dir, datafile), 'r') as f:
+        #   lines = f.readlines()
+            lines = [x.decode('utf8').strip() for x in f.readlines()]
+            for line in lines[1:]:
+                i += 1
+                cookie = line.split('\t')[1]
+                if cookie in cookie_set:
+                    n += 1
+                    print(line, file = output_file)
+        
+        print(i, "lines processed - ", end='')
+        print(n, "panel cookie events found")
+        n_cookies += n
+        
+    output_file.close()    
+    print(n_cookies, "TOTAL NUMBER of panel cookie events found for day", day.strftime('%Y%m%d'))
+    
+    day += timedelta(days=1)
+    FILE_WILDCARD = "pixelview-turbo-no-porn-de." + day.strftime('%Y%m%d') + "-????.????.log.gz"
+    data_files = glob.glob(os.path.join(input_dir, FILE_WILDCARD))
+    data_files_count = len(data_files)
+    print(data_files_count, "files found for day", day.strftime('%Y%m%d'))
+
+print("*** DONE ***")
