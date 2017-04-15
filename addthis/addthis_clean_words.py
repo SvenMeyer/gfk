24de60cd5e36731c97e5553122fc03adbc5c6da0
SvenMeyer <github_dev@SvenMeyer.com>
2017-04-15 11:43:10 +0200

    read addthis raw log file, clean tag columns, create words column

--- addthis/addthis_clean_words.py
+++ addthis/addthis_clean_words.py
@@ -1,8 +1,20 @@
 '''
 read addthis raw text log file into pandas DataFrame
 
-df.columns :  Index(['TIMESTAMP', 'UID', 'GEO', 'URL', 'CATEGORIES', 'USERAGENT',
-       'META_KEYWORDS', 'KEY_TERMS', 'ENTITIES']
+1) INPUT :
+
+one RAW AddThis text logfile (tab-separated CSV-file)
+columns ['TIMESTAMP', 'UID', 'GEO', 'URL', 'CATEGORIES', 'USERAGENT',
+         'META_KEYWORDS', 'KEY_TERMS', 'ENTITIES']
+
+2 OUTPUT :
+
+a) full RAW AddThis text logfile (tab-separated CSV-file)
+columns ['addthis_ID','TIMESTAMP', 'UID', 'GEO', 'URL', 'USERAGENT', 'words']
+
+b) sample RAW AddThis text logfile (tab-separated CSV-file) - first 1000 lines only
+columns [addthis_ID','TIMESTAMP', 'UID', 'GEO', 'URL', 'USERAGENT',
+         'META_KEYWORDS', 'KEY_TERMS', 'ENTITIES', 'words']
 '''
 
 import os
@@ -18,10 +30,13 @@ import re
 WORK_DIR = "/media/sumeyer/SSD_2/ML_DATA/GFK/AddThis/data_panel"
 INPUT_FILE_NAME = "addthis_panel.tsv"
 
-df = pd.read_csv(os.path.join(WORK_DIR, INPUT_FILE_NAME), sep='\t', skipinitialspace=True, nrows=None, memory_map=True,
+input_file_path = os.path.join(WORK_DIR, INPUT_FILE_NAME)
+print("loading file : ", input_file_path, ' ... ', end='')
+df = pd.read_csv(input_file_path, sep='\t', skipinitialspace=True, nrows=None, memory_map=True,
                  dtype={'TIMESTAMP':np.uint64, 'UID':str, 'GEO':str, 'URL':str, 'CATEGORIES':str, 
                         'USERAGENT':str, 'META_KEYWORDS':str, 'KEY_TERMS':str, 'ENTITIES':str},
                  error_bad_lines=False, warn_bad_lines=True, quoting=csv.QUOTE_NONE) # index_col='UID',
+print('DONE')
 print("df.shape = ", df.shape)
 print("df.columns : ", df.columns)
 df.rename(columns={'UID':'addthis_ID'}, inplace=True)
@@ -34,7 +49,7 @@ word_cols = list(['CATEGORIES','META_KEYWORDS','KEY_TERMS','ENTITIES'])
 # clean word colums, remove special chars and join all words into one additional 'word'-column
 # http://stackoverflow.com/questions/26886653/pandas-create-new-column-based-on-values-from-other-columns
 def clean_words(row):
-    rc = re.compile(r'(\%\w\w)|[^\w]', re.UNICODE)
+    rc = re.compile(r'[\d]|[^\w]', re.UNICODE)
     words = rc.sub(" ", row['CATEGORIES']) + ' ' + rc.sub(" ", row['META_KEYWORDS']) + ' ' + rc.sub(" ", row['KEY_TERMS']) + ' ' + rc.sub(" ", row['ENTITIES'])
     return words
 
@@ -58,7 +73,8 @@ print("average events / cookie = ", df.shape[0] / len(df.index.unique() ))
 print(df.info())
 
 print("writing csv file")
+df[:1000].to_csv(os.path.join(WORK_DIR, 'addthis_panel_words_sample_1k.tsv'), sep='\t')
+# drop redundant columns not needed for further processing
 df.drop(word_cols, inplace=True, axis=1)
 df.to_csv(os.path.join(WORK_DIR, 'addthis_panel_words.tsv'), sep='\t')
-df[:1000].to_csv(os.path.join(WORK_DIR, 'addthis_panel_words_sample_1k.tsv'), sep='\t')
 print("DONE")
